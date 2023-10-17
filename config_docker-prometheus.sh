15dc3409b7ffac180ce17d94c43c703e96102d0f
NoahProphet <alipour.sina@protonmail.com>
2023-10-17 15:32:59 +0330

    first commit

---
+++ config_docker-prometheus.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#help : bash config_docker.sh --name_prj sanay --logstash_port 2021 --promtail_port 1514 --prometheus_port 2022
+# bash config_docker-prometheus.sh --name_prj sanay --prometheus_port 5031
+
+PASSWORD="\$"
+ARGUMENT_LIST=(
+    "name_prj"
+    "prometheus_port"
+
+)
+
+# read arguments
+opts=$(getopt \
+    --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+    --name "$(basename "$0")" \
+    --options "" \
+    -- "$@"
+)
+
+eval set --$opts
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --name_prj)
+            NAME=$2
+            shift 2
+            ;;
+
+        --prometheus_port)
+            PROM_PORT=$2
+            shift 2
+            ;;
+
+        *)
+            break
+            ;;
+    esac 
+done
+
+
+
+echo "prj_name: $NAME";
+mkdir -p $NAME/{prometheus_data,config}
+echo "$PASSWORD" | sudo -S mkdir -p /var/log/monitoring/$NAME/prometheus
+
+mkdir -p ssl
+if [ -z "$(ls -A ssl)" ]; then
+   openssl req -x509 -newkey rsa:1024 -keyout ssl/prom_node_key.pem -out ssl/prom_node_cert.pem -days 29220 -nodes -subj /commonName=prom_node/ -addext "subjectAltName=DNS:prom_node"
+   openssl req -x509 -newkey rsa:2048 -keyout ssl/prometheus_key.pem -out ssl/prometheus_cert.pem -days 29220 -nodes -subj /commonName=prometheus/ -addext "subjectAltName=DNS:prometheus"
+   echo "+make SSL files"
+else
+   echo "-SSL files exist"
+fi
+
+rm -rf $NAME/config/prometheus/ssl
+cp -r default_conf/prometheus/config/ $NAME/;
+cp -r ssl $NAME/config/prometheus
+cp default_conf/prometheus/docker-compose-prometheus.yml $NAME/docker-compose-prometheus.yml
+echo "+ copy default config";
+
+mkdir -p nginx
+if [ -z "$(ls -A nginx)" ]; then
+    cp -r default_conf/nginx/* nginx/
+    cp -r ssl nginx/data
+    wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz -o nginx/data/node_exporter-1.3.1.linux-amd64.tar.gz
+else 
+    echo "nginx files exist"
+fi
+
+
+sed -i -e "s/NAME/${NAME}/g" $NAME/docker-compose-prometheus.yml;
+sed -i -e "s/PROMETHEUS-PORT/${PROM_PORT}/g" $NAME/docker-compose-prometheus.yml;
+
+echo "$PASSWORD" | sudo -S docker-compose -f $NAME/docker-compose-prometheus.yml up -d
+echo "$PASSWORD" | sudo -S docker-compose -f $NAME/docker-compose-prometheus.yml restart
+
+echo "$PASSWORD" | sudo -S docker-compose -f nginx/docker-compose-nginx.yml up -d
+echo "$PASSWORD" | sudo -S docker-compose -f nginx/docker-compose-nginx.yml restart
+
+echo "+ docker-compose file changed."
+#netstat -na | grep -w 909 | awk '{print$4}' |grep -w 909|sed -e "s/127.0.0.1://g" | sed -e "s/://g" | sort -u
+#Run Docker compose:
+#cd  $SITE_NAME && docker-compose -f docker-compose -prometheus.yml up -d
+#sudo docker build -f Dockerfile -t bulud-waf:latest .
+
---
+++ node-exporter-linux-installer.sh
@@ -0,0 +1,159 @@
+#!/bin/sh -e
+
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
+# If an error occurs, the abort() function will be called.
+
+
+#Set Password for node-exporter
+PASSWORD_OF_SERVER="Change itt"
+SERVER_IP="Change it"
+SERVER_USER="Change it"
+
+#check root
+_check_root () {
+    if [ $(id -u) -ne 0 ]; then
+        echo "Please run as root" >&2;
+        exit 1;
+    fi
+}
+
+
+_check_internet () {
+ if hash nc 2>/dev/null; then
+     if nc -zw1 google.com 443; then
+       echo "+ we have Internet"
+     else
+       echo "- The network access to internet is down"
+       exit 1;
+     fi
+ else
+     if ping -q -c 1 -W 1 google.com >/dev/null; then
+       echo "+ we have Internet"
+     else
+       echo "- The network access to internet is down"
+       exit 1;
+     fi
+ fi
+
+}
+
+
+_check_ufw_add_rule () {
+echo "+checking ufw..."
+sudo ufw status > ufw.tmp
+if grep -q "inactive" ufw.tmp ; then
+  echo "+ ufw disable"
+else
+  echo "+ ufw enable"
+#  sudo ufw allow from $SERVER_IP to any port 9100
+fi
+rm ufw.tmp
+
+}
+
+
+_check_root
+
+_check_internet
+
+_check_ufw_add_rule
+
+
+#adduser
+sudo apt install sshpass -y
+
+#rm -r ssl
+mkdir -p /etc/node-exporter
+mkdir -p ssl
+if id node_exporter &>/dev/null; then
+    echo 'user found'
+else
+    sudo useradd -rs /bin/false node_exporter
+    echo 'user not found so make it'
+fi
+
+
+
+wget $SERVER_IP:80/data/ssl/* ssl/
+#sshpass -p "$PASSWORD_OF_SERVER" scp -r ssl/prom_node_cert.pem monitoring@$SERVER_IP:/home/monitoring/prometheus/prometheus/ssl/prom_node_cert.pem
+echo "copy ssl file."
+
+FILE_EXPORTER_SERVICE=/lib/systemd/system/node-exporter.service
+if [ -f "$FILE_EXPORTER_SERVICE" ]; then    
+  sudo systemctl stop node-exporter.service
+fi
+
+FILE_EXPORTER=node_exporter
+if [ -f "$FILE_EXPORTER" ]; then
+    sudo cp node_exporter /usr/local/bin
+else
+    wget $SERVER_IP:80/data/node_exporter-1.3.1.linux-amd64 -o node_exporter
+fi
+
+sudo cp node_exporter /usr/local/bin
+sudo cp ssl/prom_node_cert.pem /etc/node-exporter/prom_node_cert.pem
+sudo cp ssl/prom_node_key.pem /etc/node-exporter/prom_node_key.pem
+sudo cp ssl/prometheus_cert.pem /etc/node-exporter/prometheus_cert.pem
+sudo chown node_exporter /usr/local/bin/node_exporter
+sudo chmod +x /usr/local/bin/node_exporter
+#configuring node-exporter file and folder
+
+cat << EOF > /etc/node-exporter/web.yml
+tls_server_config:
+  cert_file: /etc/node-exporter/prom_node_cert.pem
+  key_file: /etc/node-exporter/prom_node_key.pem
+  client_auth_type: RequireAndVerifyClientCert
+  client_ca_file: /etc/node-exporter/prometheus_cert.pem
+
+EOF
+
+sudo chown -R node_exporter /etc/node-exporter
+if [ -x "$(command -v systemctl)" ]; then
+    cat << EOF > /lib/systemd/system/node-exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter --web.config.file=/etc/node-exporter/web.yml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload
+    systemctl enable node-exporter
+    systemctl start node-exporter
+
+else
+    echo "No known service management found" >&2;
+    exit 1;
+fi
+
+rm -r ssl
+sudo apt purge sshpass -y
+
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE *** 
+************
+'
+
